f7d8865c7a69985b9e1c13918d2aeefb066ca177
Sandesh <chapagainsandesh88@gmail.com>
2023-11-20 15:53:02 -0600

    add path to get tree rank

--- app.py
+++ app.py
@@ -13,18 +13,17 @@ sample_data_path = '../sample-data/dna.50MB'
 if (os.path.isfile(sample_data_path)):
     with open(sample_data_path) as f:
         i = 0
-        for line in f.readlines()[:4]:
+        for line in f.readlines()[:3]:
             i+=1
             line = line.strip()
             print('read line:',i,'length:',len(line))
             text += line
-
 text += META_CHAR
 sorted_chars = sorted(list(set(text+META_CHAR)))
 character_set = ''.join(sorted_chars)
 
 t1 = time()
-index = FMIndex(text,character_set=character_set,debug=True)
+index = FMIndex(text,character_set=character_set,debug=True, block_size=1)
 t2 = time()
 print(f'Index has been built on the text in {(t2-t1):.3f} seconds')
 
@@ -42,8 +41,24 @@ def main():
         t2 = time()
         return render_template('index.html',count=count, query=pattern, t_span=t2-t1,character_set=' | '.join(sorted_chars))
 
+@app.route("/rank/",methods=['GET'])
+def rank():
+    try:
+        charIdx = int(request.args.get('char_idx'))
+        idx = int(request.args.get('idx'))
+    except:
+        return f"<p> Error: char_index and idx must be provided as query params </p>"
+    t1 = time()
+    charRank = index._waveletTree.getRank(charIdx,idx)
+    t2 = time()
+    return f"<p>Rank: {index._waveletTree.getRank(charIdx,idx)}. Computed in {(t2-t1):.3f} seconds</p>"
+
 @app.route("/trends",methods=['GET'])
 def trends():
+    '''
+    Build a plot indicating how the WaveletTree getRank() method performs
+    as a function of character size (Σ) and length of length of the indexed text (n)
+    '''
     data = {
         "text_length": len(text),
         "plot_data":{
@@ -51,23 +66,19 @@ def trends():
             "ts": []
         }
     }
-
-    num_repeats = 1 # Repeat queries for the same m and take average for consistency
-    pattern_lengths =  [200,400,600,800,1000,1200]
-    characters_in_pattern = character_set[1:] # Exclude the META_CHAR from the pattern
-    for i,m in enumerate(pattern_lengths):
-        total_time = 0
-        for j in range(num_repeats):
-            shift = randint(0,len(text)-m-1)
-            pattern = text[shift:shift+m]
+    
+    # Only vary the position of the rank query, not the text
+    if request.args.get('single_text') in ['True','true']:
+        shift = 0
+        skip = 10
+        for i in range(0,300):
+            charIdx = randint(2,len(character_set)) # Exclude the META_CHAR from tests
             t1 = time()
-            index.findMatchCount(pattern)
+            index._waveletTree.getRank(1,(shift+i)*skip)
             t2 = time()
             time_elapsed = (t2-t1)
-            print(f'Trail {j+1} for m={m} finished in {time_elapsed} seconds.')
-            total_time += time_elapsed
-
-        data['plot_data']['ms'].append(m)
-        data['plot_data']['ts'].append(total_time/num_repeats)
+            data['plot_data']['ms'].append(i)
+            data['plot_data']['ts'].append(time_elapsed)
+            print(f'Index: {i}')
 
     return render_template('trends.html',jsonData=json.dumps(data))
